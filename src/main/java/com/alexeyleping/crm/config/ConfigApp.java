b31a80c9d549c624d3d12ff170b014e46e85d027
Alexey Leping <alexeyleping@yandex.ru>
2022-08-16 19:12:30 +0300

    add flyway

--- src/main/java/com/alexeyleping/crm/config/ConfigApp.java
+++ src/main/java/com/alexeyleping/crm/config/ConfigApp.java
@@ -1,15 +1,8 @@
 package com.alexeyleping.crm.config;
 
 import org.springframework.context.annotation.Configuration;
-import java.util.Properties;
 
 @Configuration
 public class ConfigApp {
-    final Properties hibernateProperties() {
-        final Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        hibernateProperties.setProperty("hibernate.show_sql", "true");
-        return hibernateProperties;
-    }
+
 }
