42a403abb47f29c331996e67072856829c68c343
Alexey Leping <aleping@yandex.ru>
2022-12-08 21:59:19 +0300

    add routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,20 @@
 import React, {Component} from "react";
 import {Button, Container, Form, FormControl, Nav, Navbar, NavbarBrand, NavLink} from "react-bootstrap";
 import logo from './logo192.png'
+import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import Start from "../pages/Start"
+import Queue from "../pages/Queue"
+import Contacts from "../pages/Contacts"
+import AboutUs from "../pages/AboutUs"
 
 export default class Header extends Component {
     render() {
         return(
+            <Router>
             <div>
                 <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand href="/">
+                        <Navbar.Brand href="/start">
                             <img
                                 src={logo}
                                 height="30"
@@ -20,7 +26,7 @@ export default class Header extends Component {
                         <Navbar.Toggle aria-control="responsive-navbar-nav" />
                         <Navbar.Collapse id ="responsive-navbar-nav">
                             <Nav className="ml-auto" >
-                                <Nav.Link href="/">Start</Nav.Link>
+                                <Nav.Link href="/start">Start</Nav.Link>
                                 <Nav.Link href="/queue">Queue</Nav.Link>
                                 <Nav.Link href="/contacts">Contacts</Nav.Link>
                                 <Nav.Link href="/about">About Us</Nav.Link>
@@ -36,7 +42,14 @@ export default class Header extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+                <Routes>
+                    <Route path="/start" element={<Start />} />
+                    <Route path="/queue" element={<Queue />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    <Route path="/about" element={<AboutUs />} />
+                </Routes>
             </div>
+            </Router>
         )
     }
 }
